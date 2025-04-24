81a4ac20800ecbddf456178e94b2bcd7a1e52d95
Eric Lundby <Eric.Lundby@gmail.com>
2025-04-24 09:45:14 -0600

    Give windows a shot.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,25 +1,23 @@
 @echo on
 SetLocal EnableDelayedExpansion
 
-:: flang still uses a temporary name not recognized by CMake
-copy %BUILD_PREFIX%\Library\bin\flang-new.exe %BUILD_PREFIX%\Library\bin\flang.exe
-
 mkdir build
 cd build
 
 if "%USE_OPENMP%"=="1" (
-    REM https://discourse.cmake.org/t/how-to-find-openmp-with-clang-on-macos/8860
-    set "CMAKE_EXTRA=-DOpenMP_ROOT=%LIBRARY_LIB%"
-    REM not picked up by `find_package(OpenMP)` for some reason
-    set "CMAKE_EXTRA=-DOpenMP_Fortran_FLAGS=-fopenmp -DOpenMP_Fortran_LIB_NAMES=libomp"
+    set "CMAKE_EXTRA=^
+    -DOpenMP_ROOT=%LIBRARY_LIB% ^
+    -DOpenMP_Fortran_FLAGS=/Qopenmp ^
+    -DOpenMP_Fortran_LIB_NAMES=libiomp5md ^
+    -DOpenMP_Fortran_LIBRARIES=%LIBRARY_LIB%\libiomp5md.lib"
 )
 
 :: millions of lines of warnings with clang-19
 set "CFLAGS=%CFLAGS% -w"
 
 cmake -G "Ninja"                            ^
-    -DCMAKE_C_COMPILER=clang-cl             ^
-    -DCMAKE_Fortran_COMPILER=flang          ^
+    -DCMAKE_C_COMPILER=icx             ^
+    -DCMAKE_Fortran_COMPILER=ifx          ^
     -DCMAKE_BUILD_TYPE=Release              ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DDYNAMIC_ARCH=ON                       ^
