d31552239a3e626090f4346e69e42d3b380d8322
Eric Lundby <Eric.Lundby@gmail.com>
2026-06-25 14:03:36 -0600

    standardize on using %target_platform%

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -14,7 +14,7 @@ if "%USE_OPENMP%"=="1" (
 :: millions of lines of warnings with clang-19
 set "CFLAGS=%CFLAGS% -w"
 
-if /i "%SUBDIR%"=="win-arm64" (
+if /i "%target_platform%"=="win-arm64" (
     REM getarch incorrectly chooses CORE=A64FX for arm64, so we need to force it to ARMV8
     set "CMAKE_ARCH_ARGS=-DDYNAMIC_ARCH=OFF -DTARGET=ARMV8"
 ) else (
